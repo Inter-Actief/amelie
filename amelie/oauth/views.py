b326d1b813920b68a120ab399d3e8e096d84377d
Kevin Alberts <kevin@kevinalberts.nl>
2023-04-02 08:48:37 +0200

    oauth: Fix oauth request url

--- amelie/oauth/views.py
+++ amelie/oauth/views.py
@@ -1,66 +1,9 @@
-from django.contrib.auth import login
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
+from django.shortcuts import render
 from django.views.generic import FormView
 
 from amelie.iamailer import MailTask, Recipient
-from amelie.members.models import Person
 from amelie.oauth.forms import OAuth2RequestForm
-from amelie.oauth.models import LoginToken
 from amelie.settings import DEFAULT_FROM_EMAIL
-from amelie.tools.decorators import require_board
-from amelie.tools.mail import PersonRecipient
-from django.utils.translation import gettext_lazy as _
-
-
-def token_login(request, token):
-    try:
-        deadline = timezone.now() - LoginToken.EXPIRE_AFTER
-        token = LoginToken.objects.get(token=token, date__gte=deadline)
-    except LoginToken.DoesNotExist:
-        return render(request=request,
-                      template_name="403.html",
-                      context={'reason': _('The login token does not exist or is expired.')},
-                      status=403)
-    else:
-        if token.person.user is None:
-            return render(request=request,
-                          template_name="403.html",
-                          context={'reason': _('Your account is currently not linked to a user. '
-                                               'Please contact the board.')},
-                          status=403)
-        login(request, token.person.user, backend="amelie.tools.auth.IALoginBackend")
-        token.delete()
-
-        return render(request, 'token_login.html', {})
-
-
-def create_token_and_send_email(request, person):
-    token = LoginToken()
-    token.person = person
-    token.save()
-
-    url = request.build_absolute_uri(token.get_absolute_url())
-
-    task = MailTask(template_name="send_token.mail",
-                    report_to=request.person.email_address,
-                    report_always=False)
-
-    task.add_recipient(PersonRecipient(
-        recipient=person,
-        context={"url": url}
-    ))
-
-    task.send()
-
-
-@require_board
-def send_token(request, person_id):
-    person = get_object_or_404(Person, id=person_id)
-    create_token_and_send_email(request, person)
-    return HttpResponse(_("Sent"))
-
 
 class RequestOAuth(FormView):
     template_name = "request_oauth.html"
