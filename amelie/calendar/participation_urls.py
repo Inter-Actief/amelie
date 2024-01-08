487274dc172eb446b9a87e5044c85f830e64f629
supertom01 <meulenkampt@gmail.com>
2024-01-08 19:50:26 +0100

    Resolve RemovedInDjango40Warning

--- amelie/calendar/participation_urls.py
+++ amelie/calendar/participation_urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from amelie.calendar import views
 
 app_name = 'calendar'
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/pay/', views.PayParticipationView.as_view(), name='pay_participation'),
+    path(r'<int:pk>/pay/', views.PayParticipationView.as_view(), name='pay_participation'),
 ]
