0b36232eab8c9446dc43cbe0c2145d6ad84c97b6
supertom01 <meulenkampt@gmail.com>
2024-02-05 20:41:04 +0100

    Add a new graph type

--- amelie/members/static/js/dogroup_tree.js
+++ amelie/members/static/js/dogroup_tree.js
@@ -15,6 +15,9 @@ var state = {
 
 let dogroups;
 
+let gravityConstant = 0.9;
+let forceConstant = 100;
+let differenceConstant = 500;
 
 const config = {
     gap_x: 1,
@@ -49,6 +52,67 @@ async function init() {
    dogroups = response;
 }
 
+function determineForces(nodes, connections) {
+    // apply force between nodes
+    nodes.forEach(node => {
+        node.force_x = node.x * -1 * gravityConstant;
+        node.force_y = node.y * -1 * gravityConstant;
+    });
+
+    // apply recursive force between nodes
+    for (let i = 0; i < nodes.length; i++) {
+        for (let j = i + 1; j < nodes.length; j++) {
+            const node1 = nodes[i];
+            const node2 = nodes[j];
+
+            const pos_x = node1.x;
+            const pos_y = node1.y;
+            const dir_x = node2.x - pos_x;
+            const dir_y = node2.y - pos_y;
+            let force_x = dir_x / (Math.sqrt(dir_x ** 2 + dir_y ** 2) ** 2);
+            let force_y = dir_y / (Math.sqrt(dir_x ** 2 + dir_y ** 2) ** 2);
+            force_x *= forceConstant;
+            force_y *= forceConstant;
+
+            nodes[i].force_x += force_x * -1;
+            nodes[i].force_y += force_y * -1;
+            nodes[j].force_x += force_x;
+            nodes[j].force_y += force_y;
+        }
+    }
+
+    // apply forces to the edges.
+    for (let i = 0; i < nodes.length; i++) {
+        for (let j = i + 1; j < nodes.length; j++) {
+            let a = null;
+            let b = null;
+
+            if (!!connections[nodes[i].key] && !!connections[nodes[i].key][nodes[j].key]) {
+                a = nodes[i];
+                b = nodes[j];
+            } else if (!!connections[nodes[j].key] && !!connections[nodes[j].key][nodes[i].key]) {
+                a = nodes[j];
+                b = nodes[i];
+            } else {
+                // If dogroups are not associated with each other, then push them apart.
+                nodes[i].force_x -= differenceConstant * .1;
+                nodes[i].force_y -= differenceConstant * .1;
+                nodes[j].force_x += differenceConstant * .1;
+                nodes[j].force_y += differenceConstant * .1;
+                continue;
+            }
+
+            // const diff_x = a.x - b.x;
+            // const diff_y = a.y - b.y;
+            const difference = connections[a.key][b.key] * differenceConstant;
+            nodes[i].force_x += difference;
+            nodes[i].force_y += difference;
+            nodes[j].force_x -= difference;
+            nodes[j].force_y -= difference;
+        }
+    }
+}
+
 function createDoGroupGraph() {
     let graph = new graphology.Graph();
 
@@ -57,6 +121,8 @@ function createDoGroupGraph() {
      * @type {{number: {number: {number}}}}
      */
     let connections = {};
+
+    // Create a connection from a dogroup to another dogroup if their parent was from that dogroup.
     dogroups.map((dogroup) => {
         if (!(dogroup.id in connections)) {
             connections[dogroup.id] = {};
@@ -71,35 +137,72 @@ function createDoGroupGraph() {
         });
     });
 
+    // Count how many connections are in total associated to this dogroup.
+    let connectionCount = {};
+    Object.keys(connections).map((dogroup) => {
+        connectionCount[dogroup] = Object.values(connections[dogroup]).reduce((accumulator, count) => accumulator + count, 0);
+    });
+
     // TODO: Figure out automated layouts: https://graphology.github.io/standard-library/layout-force.html
 
     // Add a node for each dogroup.
-    dogroups.map((dogroup, i) => {
-        graph.addNode(dogroup.id, {
-            label: dogroup.name,
-            color: dogroup.color,
-            x: i,
-            y: i,
+    let nodes = [];
+    dogroups
+        .map((dogroup, i) => {
+            nodes.push({
+                key: dogroup.id,
+                label: dogroup.name,
+                color: dogroup.color,
+                size: 1, //dogroup.generations.length,
+                x: Math.floor(i/10),
+                y: i%10,
+            });
         });
-    });
+
+    for (let i = 0; i < 10; i++) {
+        determineForces(nodes, connections);
+    }
+
+
+    // Apply the calculated forces to the node their positions.
+    for (let i = 0; i < nodes.length; i++) {
+        if (!!connectionCount[nodes[i].key]) {
+            nodes[i].x += nodes[i].force_x / connectionCount[nodes[i].key];// * nodes[i].size;
+            nodes[i].y += nodes[i].force_y / connectionCount[nodes[i].key];// * nodes[i].size;
+        } else {
+            nodes[i].x += nodes[i].force_x;// * nodes[i].size;
+            nodes[i].y += nodes[i].force_y;// * nodes[i].size;
+        }
+
+
+    }
+
+    nodes.forEach(node => {
+        graph.addNode(node.key, node);
+    })
 
     Object.keys(connections).map((source) => {
         Object.keys(connections[source]).map((target) => {
-            graph.addEdge(source, target, { size: connections[source][target] });
+            graph.addEdge(source, target, {
+                size: connections[source][target],
+            });
+
+            // console.log(connections[source][target]);
         });
     });
-
     return graph;
 }
 
 function createTreeGraph() {
     let graph = new graphology.Graph();
+
+    // Get all the available years.
     let years = dogroups
         .map((dogroup) => dogroup.generations.map((generation) => generation.year))
         .reduce((prev, curr) => {
             curr.map((year) => prev.add(year));
             return prev;
-        }, new Set())
+        }, new Set());
     years = [...years].sort((a, b) => b - a);
 
     // Give every dogroup its own x coordinate
@@ -115,7 +218,7 @@ function createTreeGraph() {
     let current_y_coord = 0;
     years.map((year) => {
         year_y_coords[year] = current_y_coord;
-       current_y_coord += config.gap_y;
+        current_y_coord += config.gap_y;
     });
 
     // Start adding in all the nodes.
