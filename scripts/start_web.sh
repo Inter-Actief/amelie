db811aca8915315956eb84fead11515e1d073f8e
Kevin Alberts <kevin@kevinalberts.nl>
2024-06-16 23:01:54 +0200

    Update start_web.sh - Give amelie more time to handle requests before committing RIP, and use the proxy headers.

--- scripts/start_web.sh
+++ scripts/start_web.sh
@@ -2,4 +2,4 @@
 
 # Start django server (ASGI)
 echo "Starting daphne server..."
-daphne -b 0.0.0.0 -p 8000 amelie.asgi:application
+daphne -b 0.0.0.0 -p 8000 --application-close-timeout 60 --proxy-headers amelie.asgi:application
