c24a6d3ce782f0e0d793f808f5a6f8ced3fdd310
Kevin Alberts <kevin@kevinalberts.nl>
2024-06-17 00:29:10 +0200

    Add wsgi start script

--- scripts/start_web_wsgi.sh
+++ scripts/start_web_wsgi.sh
@@ -2,4 +2,4 @@
 
 # Start django server (WSGI)
 echo "Starting uWSGI server..."
-uwsgi --module=amelie.wsgi:application --master -s 0.0.0.0:8080 -p 4 -t 60 --log-x-forwarded-for
+uwsgi --module=amelie.wsgi:application --master -s 0.0.0.0:8000 -p 4 --log-x-forwarded-for
