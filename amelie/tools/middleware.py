aaf98c6950f8badfd5566a7bfb5dbea517192d8f
Kevin Alberts <kevin@kevinalberts.nl>
2024-09-17 15:24:44 +0200

    middleware - If person has a language set in session (by clicking the language switcher button), do not activate their preferred language.

--- amelie/tools/middleware.py
+++ amelie/tools/middleware.py
@@ -36,5 +36,6 @@ class GlobalIAVariablesMiddleware(MiddlewareMixin):
             request.is_board = request.user.is_superuser or request.person.is_board()
             request.is_education_committee = request.is_board or request.person.is_education_committee()
 
-            preferred_language = request.person.preferred_language
-            translation.activate(preferred_language)
+            if not request.session.get(translation.LANGUAGE_SESSION_KEY, False):
+                preferred_language = request.person.preferred_language
+                translation.activate(preferred_language)
