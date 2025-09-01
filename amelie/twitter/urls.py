7e6e160d41b16e64b5c083ed7245a388ded0e70b
Kevin Alberts <kevin@kevinalberts.nl>
2025-09-01 18:24:08 +0200

    twitter: Remove last references so tests can succeed

--- amelie/twitter/urls.py
+++ amelie/twitter/urls.py
@@ -6,5 +6,4 @@ from amelie.twitter import views
 app_name = 'twitter'
 
 urlpatterns = [
-    path('', views.index, name='index'),
 ]
--- amelie/twitter/views.py
+++ amelie/twitter/views.py
@@ -4,7 +4,3 @@ from django.utils.translation import gettext_lazy as _l
 
 from amelie.tools.decorators import require_board
 
-
-@require_board
-def index(request):
-    return render(request, 'twitter_index.html', locals())
