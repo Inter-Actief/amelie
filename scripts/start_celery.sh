40440863f2688e7edd919cd7a54e41fe52d498f1
Kevin Alberts <kevin@kevinalberts.nl>
2025-09-15 21:09:08 +0200

    Fix queue name

--- scripts/start_celery.sh
+++ scripts/start_celery.sh
@@ -3,18 +3,18 @@
 # Extra command-line arguments to the worker:
 # Jelte: Concurrency op 1 gezet, zodat er geen meerdere Claudia verify-calls tegelijk gaan lopen
 # Jarmo: -E optie aangezet, zo geeft celery events door aan celerymon.
-# Kevin: -Q optie meegeven, zo kunnen we meerdere celery workers starten voor verschillende queues (mail/celery/overig)
+# Kevin: -Q optie meegeven, zo kunnen we meerdere workers starten voor verschillende queues (iamailer/claudia/overig)
 
 # Parse queue argument from CLI
 # No argument means default queue (celery)
 if [ -z "$1" ]; then
     QUEUE="celery"
 # Valid queues are "mail", "claudia" and "celery"
-elif [ "$1" = "mail" ] || [ "$1" = "claudia" ] || [ "$1" = "celery" ]; then
+elif [ "$1" = "iamailer" ] || [ "$1" = "claudia" ] || [ "$1" = "celery" ]; then
     QUEUE="$1"
 # If an invalid queue is given, stop with an error.
 else
-    echo "Error: Invalid queue value '$1'. Valid values are: mail, claudia, celery" >&2
+    echo "Error: Invalid queue value '$1'. Valid values are: iamailer, claudia, celery" >&2
     exit 1
 fi
 
