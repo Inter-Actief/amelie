74a62288242354e405c09dc8e5df49f53fdb7ad2
Kevin Alberts <kevin@kevinalberts.nl>
2025-12-19 15:55:17 +0100

    Remove unused imports and move new method to amelie.tools.http

--- amelie/settings/generic.py
+++ amelie/settings/generic.py
@@ -1,5 +1,4 @@
 # -*- coding: UTF-8 -*-
-import ipaddress
 # Note: Imports cannot have a direct dependency on the settings below.
 # Meaning, when a package is imported here, the initialization of that package cannot use the settings.
 import os
@@ -925,7 +924,7 @@ PEERTUBE_BASE_URL = "https://video.ia.utwente.nl"  # No trailing slash!
 
 # The special theme of the website, for special occasions. This overrides the theme of the website for everyone!
 # Default: None (will use the normal theme). Options: ["christmas", "valentine"]
-WEBSITE_THEME_OVERRIDE = "christmas"
+WEBSITE_THEME_OVERRIDE = None
 
 # Blocked IP addresses for showing themes (Raspberry pis don't like our themes very much)
 BLOCKED_THEME_IP_RANGES = ['130.89.190.121', '130.89.190.122']
--- amelie/style/context_processors.py
+++ amelie/style/context_processors.py
@@ -1,9 +1,7 @@
-import ipaddress
-
 from django.conf import settings
 
 from amelie.forms import AmelieAuthenticationForm
-from amelie.tools.http import get_client_ips
+from amelie.tools.http import client_has_themes_disabled
 
 
 def basis_context(request):
@@ -17,10 +15,3 @@ def theme_context(request):
     if not client_has_themes_disabled(request):
         return {'theme': None}
     return {'theme': settings.WEBSITE_THEME_OVERRIDE}
-
-
-def client_has_themes_disabled(request):
-    all_ips, real_ip = get_client_ips(request)
-    return not real_ip in settings.BLOCKED_THEME_IP_RANGES
-
-
